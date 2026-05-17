5c25838a3f1feee86912d119d079406cddb9b1af
Saar_Itr_P <saaritr.official@gmail.com>
2026-05-17 22:27:02 +0530

    HeroCarousel Image change

--- src/components/HeroCarousel.tsx
+++ src/components/HeroCarousel.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useRef } from "react";
 
-import img1 from "@/assets/Main_Page/1_Tradition_1.png";
-import img2 from "@/assets/Main_Page/2_Notes_1.png";
+import img1 from "@/assets/Main_Page/1_Tradition_2.png";
+import img2 from "@/assets/Main_Page/2_Notes_2.png";
 import img3 from "@/assets/Main_Page/3_Discovery_2.png";
 
 const originalImages = [img1, img2, img3];
